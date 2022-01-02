836c9aa496d9c29d24023e302468ca8daa78f473
usedhondacivic <usedhondacivic@users.noreply.github.com>
2022-01-02 02:31:49 +0000

    deploy: 9c7c4ac8763b7fd9c867197e297348c7792b95fe

---
+++ _next/static/_l0-4wHjdGVndRHJlP8Wu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-6b62a5dd22dcaa20.js","static/css/181359aa000976e2.css","static/chunks/pages/index-85579170a6cd6a42.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
