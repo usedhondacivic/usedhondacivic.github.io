9c7c4ac8763b7fd9c867197e297348c7792b95fe
usedhondacivic <micacer48@gmail.com>
2022-01-01 19:23:43 -0700

    Issues with build

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react'
-import dynamic from 'next/dynamic'
 
 import NoisePlanetProject from '../data/projects/Raymarching/NoisePlanetProject'
 
