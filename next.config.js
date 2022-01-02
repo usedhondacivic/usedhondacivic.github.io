03e788e90aad604f639c519157b4e364f2b77faa
usedhondacivic <micacer48@gmail.com>
2022-01-01 20:06:31 -0700

    Deploy issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/usedhondacivic.github.io/' : ''
+  reactStrictMode: true
 }
